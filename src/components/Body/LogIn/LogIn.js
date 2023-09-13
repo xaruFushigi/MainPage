bc1267292b0b55b53ddd157ebd4dc19cd59ce82f
xaruFushigi <116694089+xaruFushigi@users.noreply.github.com>
2023-09-13 22:09:00 +0900

    Update LogIn.js

--- src/components/Body/LogIn/LogIn.js
+++ src/components/Body/LogIn/LogIn.js
@@ -27,7 +27,7 @@ const LogIn = () => {
   //Log In button
   const onSubmitLogInButton = async (event) => {
     try {
-      const response = await fetch(`http://localhost:10000/auth/login`, {
+      const response = await fetch(`https://mainpage-back-end.onrender.com/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
